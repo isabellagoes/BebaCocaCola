f10773dafbe09c3544c3c2848d2ff4b0ec371f08
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-11-17 21:59:45 -0300

    beba coca cola

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.ForegroundColor = ConsoleColor.White;
+Console.BackgroundColor = ConsoleColor.Red;
+
+Console.Write(@"
+     beba  coca  cola
+     babe        cola
+     beba  coca      
+     babe  cola  caco
+     caco            
+     cola            
+          c l o a c a");
+          
+Console.ResetColor();
